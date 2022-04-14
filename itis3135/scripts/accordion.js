f2a83ec6447df176d82866d6d90b9c65c0878afd
tholley7 <tholley7@uncc.edu>
2022-04-14 19:08:14 -0400

    Convert to use jquery

--- itis3135/scripts/accordion.js
+++ itis3135/scripts/accordion.js
@@ -1,14 +1,5 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
+$( function() {
+    $("#accordion").accordion({
+        collapsible: true
     });
-}
+});
